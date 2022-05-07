2257af0a4a1c9dc13c87c487f8cdf92974200957
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-05-07 10:31:01 +0530

    Increment using redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,18 +5,18 @@ const Counter = () => {
   const counter = useSelector(state=>state.counter);
   const dispatch = useDispatch();
   const toggleCounterHandler = () => {
-    dispatch({type:'decrementby2'});
+    dispatch({type:'incrementby5'});
   };
   const toggleCounterHandler1 = () => {
-    dispatch({type:'incrementby2'});
+    dispatch({type:'decrementby5'});
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-      <button onClick={toggleCounterHandler1}>Increment By 2</button>
+      <button onClick={toggleCounterHandler}>Increment By 5</button>
+      <button onClick={toggleCounterHandler1}>Decrement By 5</button>
     </main>
   );
 };
